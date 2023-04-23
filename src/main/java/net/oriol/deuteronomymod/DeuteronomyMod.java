a2540650291a2db89446dec333bd841627a2eb85
Oriol <oriol.cuma@gmail.com>
2023-04-23 16:04:10 +0200

    addition of the code

---
+++ src/main/java/net/oriol/deuteronomymod/DeuteronomyMod.java
@@ -0,0 +1,15 @@
+package net.oriol.deuteronomymod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeuteronomyMod implements ModInitializer {
+	public static final String MOD_ID = "deuteronomymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/oriol/deuteronomymod/DeuteronomyModClient.java
@@ -0,0 +1,10 @@
+package net.oriol.deuteronomymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeuteronomyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
